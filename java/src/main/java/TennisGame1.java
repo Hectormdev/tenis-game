8da28a5e36ff8c3c10e821f80602ddf376a4a476
hectormdev <hectormarm@ext.inditex.com>
2023-09-29 15:19:01 +0100

    feat: initial changes

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -1,76 +1,68 @@
+import java.util.Objects;
 
 public class TennisGame1 implements TennisGame {
     
-    private int m_score1 = 0;
-    private int m_score2 = 0;
-    private String player1Name;
-    private String player2Name;
+    private int scorePlayer1 = 0;
+    private int scorePlayer2 = 0;
+    private final String player1Name;
 
     public TennisGame1(String player1Name, String player2Name) {
         this.player1Name = player1Name;
-        this.player2Name = player2Name;
     }
 
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
-            m_score1 += 1;
+        if (Objects.equals(playerName, player1Name))
+            scorePlayer1 += 1;
         else
-            m_score2 += 1;
+            scorePlayer2 += 1;
     }
 
     public String getScore() {
-        String score = "";
-        int tempScore=0;
-        if (m_score1==m_score2)
+        if (scorePlayer1 == scorePlayer2)
         {
-            switch (m_score1)
-            {
-                case 0:
-                        score = "Love-All";
-                    break;
-                case 1:
-                        score = "Fifteen-All";
-                    break;
-                case 2:
-                        score = "Thirty-All";
-                    break;
-                default:
-                        score = "Deuce";
-                    break;
-                
-            }
+            return getEqualScoreText();
         }
-        else if (m_score1>=4 || m_score2>=4)
+        else if (scorePlayer1 >=4 || scorePlayer2 >=4)
         {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
+            return getWinner();
         }
         else
         {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
-            }
+            String score = getTextScore(scorePlayer1);
+            score +="-";
+            score += getTextScore(scorePlayer2);
+            return score;
+
         }
+    }
+
+    private static String getTextScore(int tempScore) {
+        return switch(tempScore)
+        {
+            case 0->"Love";
+            case 1-> "Fifteen";
+            case 2 -> "Thirty";
+            case 3 ->"Forty";
+            default -> throw new IllegalStateException("Unexpected value: " + tempScore);
+        };
+    }
+
+    private String getWinner() {
+        String score;
+        int minusResult = scorePlayer1 - scorePlayer2;
+        if (minusResult==1) score ="Advantage player1";
+        else if (minusResult ==-1) score ="Advantage player2";
+        else if (minusResult>=2) score = "Win for player1";
+        else score ="Win for player2";
         return score;
+    }
+
+    private String getEqualScoreText() {
+        return switch (scorePlayer1) {
+            case 0 -> "Love-All";
+            case 1 -> "Fifteen-All";
+            case 2 -> "Thirty-All";
+            default -> "Deuce";
+        };
     }
 }
